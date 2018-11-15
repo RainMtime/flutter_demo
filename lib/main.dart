4ffea22169dfc18155409c1650532b85f4c9f1da
chunyu <746431278@qq.com>
2018-11-15 16:37:24 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return new MaterialApp(
+     title: 'hello chunyu',
+     home: new RandomWords(),
+   );
+  }
+}
+
+class RandomWordState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize:18.0);
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('hello world'),
+      ) ,
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions(){
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+
+      itemBuilder: (BuildContext _context, int i){
+        if(i.isOdd){
+           return new Divider();
+        }
+
+        final int index = i ~/ 2;
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(100));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair){
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+}
+
+class RandomWords extends StatefulWidget {
+  RandomWordState createState() => new RandomWordState();
+}
